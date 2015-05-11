b593cac6fa0fda46cb59e1d121e1e9cbf7074268
mmmmm <mike@mediasilo.com>
2015-05-11 10:06:30 -0400

    SSL connections to phoenix

--- tracking.js
+++ tracking.js
@@ -2,8 +2,8 @@ var MediaSiloAnalytics = MediaSiloAnalytics || {};
 MediaSiloAnalytics.Tracking = (function () {
 
 	// Thanks to: https://github.com/robertodecurnex/J50Npi
-	var J50Npi = {  
-	    currentScript: null,  
+	var J50Npi = {
+	    currentScript: null,
 	    getJSON: function(url, data, callback) {
 
 	      var src = url + (url.indexOf("?")+1 ? "&" : "?");
@@ -15,28 +15,28 @@ MediaSiloAnalytics.Tracking = (function () {
 
 	      data["_ms_callback"] = "MediaSiloAnalytics.J50Npi.success";
 	      for(var param_name in data){
-	          params.push(param_name + "=" + encodeURIComponent(data[param_name]));  
+	          params.push(param_name + "=" + encodeURIComponent(data[param_name]));
 	      }
 	      src += params.join("&")
 
-	      newScript.type = "text/javascript";  
+	      newScript.type = "text/javascript";
 	      newScript.src = src;
 
 	      if(this.currentScript) {
 	        head.removeChild(this.currentScript);
 	      }
-	      head.appendChild(newScript); 
+	      head.appendChild(newScript);
 	      this.currentScript = newScript;
 	    },
 	    success: null
-	}; 
+	};
 
 	var sendEvent = function(eventName, data) {
 		var data = data || {};
-		data["_ms_host"] = window.location.host; 
+		data["_ms_host"] = window.location.host;
 		data["_ms_path"] = window.location.pathname + window.location.hash;
 		data["_ms_AnalyticsTrackingKey"] = _ms_AnalyticsTrackingKey;
-        var url = "http://p-api-new.mediasilo.com/v3/analytics/"+eventName;
+        var url = "https://p-api-new.mediasilo.com/v3/analytics/"+eventName;
 		J50Npi.getJSON(url, data);
 	};
 
